6a8820163bd359e75a182f3477fc3c231a905d43
Chloe Ciora <chloeciora@gmail.com>
2018-01-28 12:38:23 -0500

    delete clones

--- sheInnovates18/mysite/polls/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-urlpatterns = patterns(
-    '',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'mysite.views.index')
-)
